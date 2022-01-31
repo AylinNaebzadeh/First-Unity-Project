6b02c893f6a21d26348fc86e9371258fe1dfecb6
bavan <bavan1379@gmail.com>
2022-02-01 00:24:25 +0330

    fix clone bugs

--- FirstProject/Assets/Scripts/targetScript.cs
+++ FirstProject/Assets/Scripts/targetScript.cs
@@ -6,25 +6,29 @@ public class targetScript : MonoBehaviour
 {
     [SerializeField] float hitPrize = 0;
     public GameObject go;
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(cycleGenerator());
-        // GameObject newCycle = Instantiate(go, new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0), Quaternion.identity);
+        //GameObject newCycle = Instantiate(go, new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0), Quaternion.identity);
     }
 
     void Update()
     {
-        
+
     }
 
     IEnumerator cycleGenerator()
     {
         while (true)
         {
-            GameObject newCycle = Instantiate(go, new Vector3(Random.Range(-10, 10), Random.Range(-5, 5), 0), Quaternion.identity);
-            Destroy(newCycle.gameObject, 0.5f);
-            yield return new WaitForSeconds(20);
+            
+            GameObject newCycle = Instantiate(go, new Vector3(Random.Range(282, 299), Random.Range(-4, 4), 0), Quaternion.identity);
+            newCycle.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas").transform, false);
+            Destroy(newCycle.gameObject, 10);
+            yield return new WaitForSeconds(1);
+
         }
     }
 
@@ -34,4 +38,6 @@ public class targetScript : MonoBehaviour
         hitPrize += 20;
         Debug.Log("You have earned new score!, your currnet score is equal to = "+ hitPrize);
     }
+
+
 }
